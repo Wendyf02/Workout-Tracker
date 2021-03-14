20c517dd1aa07db3cf003c03c6c822c355ef8612
Wendy Fgigueroa <camomila444@gmail>
2021-03-13 19:18:17 -0500

    working on route

--- routes/api.js
+++ routes/api.js
@@ -2,5 +2,35 @@ const router =  require("express").Router();
 const Workout = require("../models/workout.js");
 
 router.post("/api/workouts", ({body}, res)=> {
-    
-})
+    Workout.create(body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+          res.status(400).json(err);
+    });
+
+});
+
+router.put("/api/workouts/:id", ({body, params}, res ) => {
+    console.log(body)
+    Workout.findOneAndUpdate(params.id,
+       {$push: { exercises: body} }, { new: true })
+       .then(dbWorkout => {
+           res.json(dbWorkout);
+       })
+       .catch(err => {
+             res.status(400).json(err);
+       });
+
+});
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
 );
 
 
-// app.use(require("./routes/api.js"));
+app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
 
 
